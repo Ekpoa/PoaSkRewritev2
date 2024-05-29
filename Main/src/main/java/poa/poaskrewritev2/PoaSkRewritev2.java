50d90f14a477f7748ad404d775214715de2caf00
Ekpoa <banapires16@gmail.com>
2024-05-29 12:32:47 +0100

    Updated

--- Main/src/main/java/poa/poaskrewritev2/PoaSkRewritev2.java
+++ Main/src/main/java/poa/poaskrewritev2/PoaSkRewritev2.java
@@ -3,8 +3,10 @@ package poa.poaskrewritev2;
 import ch.njol.skript.Skript;
 import ch.njol.skript.SkriptAddon;
 import lombok.Getter;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import poa.poaskrewritev2.events.JoinLeave;
+import poa.poaskrewritev2.expressions.ExprHostname;
 
 import java.io.IOException;
 
@@ -24,7 +26,9 @@ public final class PoaSkRewritev2 extends JavaPlugin {
         SkriptAddon skriptAddon = Skript.registerAddon(this);
 
 
-        getServer().getPluginManager().registerEvents(new JoinLeave(), this);
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(new JoinLeave(), this);
+        pm.registerEvents(new ExprHostname(), this);
 
         try {
             skriptAddon.loadClasses("poa.poaskrewritev2", "expressions", "effects", "effects.entity", "effects.packets", "effects.fun"); //todo add events back
