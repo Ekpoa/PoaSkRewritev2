98e3fff302a1c7ce2c566af3b46d4a8df18f0c12
BuildTools <unconfigured@null.spigotmc.org>
2025-01-27 17:45:48 +0000

    updated

--- nms1214/src/main/java/poa/packets/TeleportPacket1214.java
+++ nms1214/src/main/java/poa/packets/TeleportPacket1214.java
@@ -3,31 +3,21 @@ package poa.packets;
 import io.netty.buffer.Unpooled;
 import lombok.SneakyThrows;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.game.ClientboundEntityPositionSyncPacket;
 import net.minecraft.network.protocol.game.ClientboundTeleportEntityPacket;
+import net.minecraft.world.entity.PositionMoveRotation;
+import net.minecraft.world.phys.Vec3;
 
 import java.lang.reflect.Constructor;
 
 public class TeleportPacket1214 {
 
-    private static Constructor<?> packetConstructor;
-
     @SneakyThrows
     public static Object teleportPacket(int id, double x, double y, double z, float yaw, float pitch, boolean onGround) {
-        FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
-        buf.writeVarInt(id);
-        buf.writeDouble(x);
-        buf.writeDouble(y);
-        buf.writeDouble(z);
-        buf.writeByte((byte) ((int) (yaw * 256.0F / 360.0F)));
-        buf.writeByte((byte) ((int) (pitch * 256.0F / 360.0F)));
-        buf.writeBoolean(onGround);
+        yaw = (yaw * 256.0F / 360.0F);
+        pitch = (pitch * 256.0F / 360.0F);
 
-        if(packetConstructor == null) {
-            packetConstructor = ClientboundTeleportEntityPacket.class.getDeclaredConstructor(FriendlyByteBuf.class);
-            packetConstructor.setAccessible(true);
-        }
-
-        return packetConstructor.newInstance(buf);
+        return new ClientboundEntityPositionSyncPacket(id, new PositionMoveRotation(new Vec3(x,y,z), new Vec3(0,0,0), yaw, pitch), onGround);
     }
 
 }
