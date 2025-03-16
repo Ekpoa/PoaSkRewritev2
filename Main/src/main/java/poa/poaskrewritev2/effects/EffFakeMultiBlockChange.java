bdc419acde1f1ffaf4da20e937833fe9a3da80b1
sovdee <10354869+sovdeeth@users.noreply.github.com>
2025-03-16 18:38:33 -0400

    Switch to using locations to avoid block loading (#28)

--- Main/src/main/java/poa/poaskrewritev2/effects/EffFakeMultiBlockChange.java
+++ Main/src/main/java/poa/poaskrewritev2/effects/EffFakeMultiBlockChange.java
@@ -19,38 +19,40 @@ import java.util.Map;
 public class EffFakeMultiBlockChange extends Effect {
 
     static {
-        Skript.registerEffect(EffFakeMultiBlockChange.class, "fake multi block set %blocks% as %blockdata% for %players%");
+        Skript.registerEffect(EffFakeMultiBlockChange.class, "fake multi block set %locations% (as|to) %blockdata% for %players%");
     }
 
-    private Expression<Block> blocks;
+    private Expression<Location> locations;
     private Expression<BlockData> blockData;
     private Expression<Player> players;
 
     @SuppressWarnings({"unchecked", "NullableProblems"})
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-        blocks = (Expression<Block>) exprs[0];
+        locations = (Expression<Location>) exprs[0];
         blockData = (Expression<BlockData>) exprs[1];
         players = (Expression<Player>) exprs[2];
         return true;
     }
 
     @Override
-    protected void execute(Event e) {
-        BlockData blockData = this.blockData.getSingle(e);
+    protected void execute(Event event) {
+        BlockData blockData = this.blockData.getSingle(event);
         if (blockData == null) return;
 
         Map<Location, BlockData> map = new HashMap<>();
-        for (Block block : blocks.getArray(e))
-            map.put(block.getLocation(), blockData);
+        for (Location location : locations.getArray(event))
+            map.put(location, blockData);
+        
+        if (map.isEmpty()) return;
 
-        for (Player player : players.getArray(e))
+        for (Player player : players.getArray(event))
             player.sendMultiBlockChange(map, false);
     }
 
     @Override
     public @NotNull String toString(@Nullable Event event, boolean debug) {
-        String blocks = this.blocks.toString(event, debug);
+        String blocks = this.locations.toString(event, debug);
         String data = this.blockData.toString(event, debug);
         String players = this.players.toString(event, debug);
         return "fake multi block set " + blocks + " as " + data + " for " + players;
